613b74b01b375121269a93f7082f0f3cf85187ac
sourlemon207 <jw1756@protonmail.com>
2020-11-28 11:50:10 +0000

    modified:   benches/basic.rs

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,13 +1,13 @@
-#![feature(test)] 
+#![feature(test)]
 extern crate test;
-use test::{Bencher};
-use std::sync::Mutex;
-use std::fs;
 use huffman_codec::Codec;
+use std::fs;
+use std::sync::Mutex;
+use test::Bencher;
 
 /* Copied from criterion-rs */
-fn black_box<T>(dummy: T) -> T{
-    unsafe{
+fn black_box<T>(dummy: T) -> T {
+    unsafe {
         let ret = std::ptr::read_volatile(&dummy);
         std::mem::forget(dummy);
         ret
@@ -15,25 +15,23 @@ fn black_box<T>(dummy: T) -> T{
 }
 
 /* Should probably change this to have a static cache */
-fn read_testfile() -> std::io::Result<String>{
+fn read_testfile() -> std::io::Result<String> {
     fs::read_to_string("./benches/testfile.txt")
 }
 
 #[bench]
-fn encode(b: &mut Bencher){
+fn encode(b: &mut Bencher) {
     let data = read_testfile().unwrap();
     let encoder = Codec::new(&data);
-    b.iter(|| {
-        black_box(encoder.encode(black_box(&data)))
-    })
+    b.iter(|| black_box(encoder.encode(black_box(&data))))
 }
 
 #[bench]
-fn decode(b: &mut Bencher){
+fn decode(b: &mut Bencher) {
     let data = read_testfile().unwrap();
     let decoder = Codec::new(&data);
     let data_to_decode: Vec<u8> = data.into();
-    b.iter(||{
+    b.iter(|| {
         black_box(decoder.decode(black_box(data_to_decode.clone())));
     })
 }
